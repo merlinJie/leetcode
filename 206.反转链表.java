dfe9561a826a9eb256d248843984345a1ff00c22
liuweijie <liuweijievip@gmail.com>
2020-03-02 10:40:45 +0800

    3.2

--- 206.反转链表.java
+++ 206.反转链表.java
@@ -15,8 +15,17 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
-      
+        if(head == null || head.next == null ) {
+            return head;
+        }
+        ListNode pre = null;
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head =  temp;
+        }
+        return pre;
     }
 }
 // @lc code=end
