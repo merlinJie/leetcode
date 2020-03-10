5f7b74cb598ae1932a6bbcb611b4d9ab8f947bc4
liuweijie <liuweijievip@gmail.com>
2020-03-10 23:18:00 +0800

    3.10

--- 509.斐波那契数.java
+++ 509.斐波那契数.java
@@ -56,13 +56,20 @@
 
 // @lc code=start
 class Solution {
+    int[] res;
     public int fib(int N) {
+        res = new int[N + 1];
         return getReslut(N);
     }
 
     public int getReslut(int n) {
         if(n == 0 || n == 1) return n;
-        return getReslut(n - 1) + getReslut(n - 2);
+        res[0] = 0;
+        res[1] = 1;
+        for(int i = 2; i < n; i++) {
+            res[i] = res[i - 1] + res[i - 2];
+        }
+        return res[n - 1] + res[n - 2];
     }
 }
 // @lc code=end
