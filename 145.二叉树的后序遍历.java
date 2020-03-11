7c761c4f4151770a82fc0ca0a95796b252b5117a
liuweijie <liuweijievip@gmail.com>
2020-03-11 22:29:57 +0800

    3.11

--- 145.二叉树的后序遍历.java
+++ 145.二叉树的后序遍历.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
 /*
  * @lc app=leetcode.cn id=145 lang=java
  *
@@ -42,7 +49,21 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-
+        Stack<TreeNode> stack = new Stack<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        if(root == null) return list;
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()) {
+            if(cur != null) {
+                stack.add(cur);
+                list.addFirst(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode node = stack.pop();
+                cur = node.left;
+            }
+        }
+        return list;
     }
 }
 // @lc code=end
