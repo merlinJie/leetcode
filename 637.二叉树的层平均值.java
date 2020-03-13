785bef7c1c8fc83d3d0ec0c1fb2b841c57057fd9
liuweijie <liuweijievip@gmail.com>
2020-03-13 23:24:36 +0800

    3.13

---
+++ 637.二叉树的层平均值.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ *
+ * [637] 二叉树的层平均值
+ *
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/description/
+ *
+ * algorithms
+ * Easy (63.41%)
+ * Likes:    98
+ * Dislikes: 0
+ * Total Accepted:    14.7K
+ * Total Submissions: 23.2K
+ * Testcase Example:  '[3,9,20,15,7]'
+ *
+ * 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组.
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 输出: [3, 14.5, 11]
+ * 解释:
+ * 第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 节点值的范围在32位有符号整数范围内。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Double> list = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            double count = 0.0;
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                count = count + cur.val;
+                if(cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if(cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            list.add(count / size);
+        }
+        return list;
+    }
+}
+// @lc code=end
+
