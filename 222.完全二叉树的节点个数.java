1b0e054b1712c37b2e14294db3cfb727e79eee2d
liuweijie <liuweijievip@gmail.com>
2020-03-16 00:01:25 +0800

    3.16

---
+++ 222.完全二叉树的节点个数.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ *
+ * [222] 完全二叉树的节点个数
+ *
+ * https://leetcode-cn.com/problems/count-complete-tree-nodes/description/
+ *
+ * algorithms
+ * Medium (67.29%)
+ * Likes:    125
+ * Dislikes: 0
+ * Total Accepted:    16.1K
+ * Total Submissions: 23.8K
+ * Testcase Example:  '[1,2,3,4,5,6]'
+ *
+ * 给出一个完全二叉树，求出该树的节点个数。
+ * 
+ * 说明：
+ * 
+ * 
+ * 完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第
+ * h 层，则该层包含 1~ 2^h 个节点。
+ * 
+ * 示例:
+ * 
+ * 输入: 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * ⁠/ \  /
+ * 4  5 6
+ * 
+ * 输出: 6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int count = 0;
+        while(!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            count++;
+            if(cur.left != null) {
+                queue.add(cur.left);
+            }
+            if(cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
+
