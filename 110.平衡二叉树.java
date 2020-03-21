619edc26ffd7e43c71c5487d41c799df70bafcc0
liuweijie <liuweijievip@gmail.com>
2020-03-22 01:26:43 +0800

    3.22

--- 110.平衡二叉树.java
+++ 110.平衡二叉树.java
@@ -63,22 +63,22 @@
  * }
  */
 class Solution {
-    boolean result = true;
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
-        calDeep(root, 0);
-        return result;
+        return calDeep(root) != -1;
     }
 
-    public int calDeep(TreeNode root, int deep) {
-        if(root == null) return deep;
-        int left = calDeep(root.left, deep + 1);
-        int right = calDeep(root.right, deep + 1);
+    public int calDeep(TreeNode root) {
+        if(root == null) return 0;
+        int left = calDeep(root.left);
+        if(left == -1) return -1;
+        int right = calDeep(root.right);
+        if(right == -1) return -1;
         if(!(Math.abs(left - right) <= 1)) {
-            result = false;
+            return -1;
         }
-        return Math.max(left, right);
+        return Math.max(left, right) + 1;
     }
-}}
+}
 // @lc code=end
 
