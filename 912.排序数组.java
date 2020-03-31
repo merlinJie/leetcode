83915e3203c2f081cae14cf345a326332e280ec6
liuweijie <liuweijievip@gmail.com>
2020-03-31 22:35:27 +0800

    3.31

---
+++ 912.排序数组.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=912 lang=java
+ *
+ * [912] 排序数组
+ *
+ * https://leetcode-cn.com/problems/sort-an-array/description/
+ *
+ * algorithms
+ * Medium (53.35%)
+ * Likes:    98
+ * Dislikes: 0
+ * Total Accepted:    46.9K
+ * Total Submissions: 79.2K
+ * Testcase Example:  '[5,2,3,1]'
+ *
+ * 给你一个整数数组 nums，请你将该数组升序排列。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [5,2,3,1]
+ * 输出：[1,2,3,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [5,1,1,2,0,0]
+ * 输出：[0,0,1,1,2,5]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 50000
+ * -50000 <= nums[i] <= 50000
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+// @lc code=end
+
