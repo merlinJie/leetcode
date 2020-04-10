715187d01ffe5d225b58fb2787bffcc22e523204
liuweijie <liuweijievip@gmail.com>
2020-04-10 22:17:26 +0800

    4.10

---
+++ 530.二叉搜索树的最小绝对差.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=530 lang=java
+ *
+ * [530] 二叉搜索树的最小绝对差
+ *
+ * https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/description/
+ *
+ * algorithms
+ * Easy (55.98%)
+ * Likes:    96
+ * Dislikes: 0
+ * Total Accepted:    12K
+ * Total Submissions: 21.3K
+ * Testcase Example:  '[1,null,3,2]'
+ *
+ * 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：
+ * 
+ * ⁠  1
+ * ⁠   \
+ * ⁠    3
+ * ⁠   /
+ * ⁠  2
+ * 
+ * 输出：
+ * 1
+ * 
+ * 解释：
+ * 最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中至少有 2 个节点。
+ * 本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+ * 相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int min = Integer.MAX_VALUE;
+    int pre = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        findMin(root);
+        return min;
+    }
+
+    public void findMin(TreeNode root) {
+        if(root == null) return;
+        findMin(root.left);
+        if(pre < Integer.MAX_VALUE) {
+            min = Math.min(min, Math.abs(pre - root.val));
+        }
+        pre = root.val;
+        findMin(root.right);
+    }
+}
+// @lc code=end
+
