f96a068797b54d6eb13cc145d55c1b356e0db858
liuweijie <liuweijievip@gmail.com>
2020-04-11 19:56:13 +0800

    4.11

---
+++ 124.二叉树中的最大路径和.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=java
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (39.76%)
+ * Likes:    346
+ * Dislikes: 0
+ * Total Accepted:    28.3K
+ * Total Submissions: 71.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个非空二叉树，返回其最大路径和。
+ * 
+ * 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   3
+ * 
+ * 输出: 6
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [-10,9,20,null,null,15,7]
+ * 
+ * -10
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 
+ * 输出: 42
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ret = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        getMax(root);
+        return ret;
+    }
+
+    public int getMax(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = Math.max(0, getMax(root.left));
+        int right = Math.max(0, getMax(root.right));
+        ret = Math.max(root.val + left + right, ret);
+        return Math.max(left + root.val, right + root.val);
+    }
+}
+// @lc code=end
+
