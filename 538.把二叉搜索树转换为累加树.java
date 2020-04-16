3f5036602b288a6778f21f6c06ca5e98e87581b1
liuweijie <liuweijievip@gmail.com>
2020-04-16 23:40:55 +0800

    4.16

---
+++ 538.把二叉搜索树转换为累加树.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=java
+ *
+ * [538] 把二叉搜索树转换为累加树
+ *
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/description/
+ *
+ * algorithms
+ * Easy (60.13%)
+ * Likes:    233
+ * Dislikes: 0
+ * Total Accepted:    21.2K
+ * Total Submissions: 35.3K
+ * Testcase Example:  '[5,2,13]'
+ *
+ * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater
+ * Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+ * 
+ * 
+ * 
+ * 例如：
+ * 
+ * 输入: 原始二叉搜索树:
+ * ⁠             5
+ * ⁠           /   \
+ * ⁠          2     13
+ * 
+ * 输出: 转换为累加树:
+ * ⁠            18
+ * ⁠           /   \
+ * ⁠         20     13
+ * 
+ * 
+ * 
+ * 
+ * 注意：本题和 1038:
+ * https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int curValue = 0;
+    public TreeNode convertBST(TreeNode root) {
+        treeGreater(root);
+        return root;
+    }
+
+    public void treeGreater(TreeNode root) {
+        if(root == null) return;
+        treeGreater(root.right);
+        root.val = curValue + root.val;
+        curValue = root.val;
+        treeGreater(root.left);
+    }
+}
+// @lc code=end
+
