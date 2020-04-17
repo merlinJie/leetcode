b757e000ca680b4826d0e922c52156c91b307a30
liuweijie <liuweijievip@gmail.com>
2020-04-17 23:31:56 +0800

    4.17

--- 617.合并二叉树.java
+++ 617.合并二叉树.java
@@ -53,27 +53,12 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if(t1 == null && t2 == null) return null;
-        if(t1 == null && t2 != null) {
-            TreeNode root =  new TreeNode(t2.val);
-            root.left = mergeTrees(null, t2.left);
-            root.right = mergeTrees(null, t2.right);
-        
-            return root;
-        }
-        if(t1 != null && t2 == null) {
-            TreeNode root =  new TreeNode(t1.val);
-            root.left = mergeTrees(t1.left, null);
-            root.right = mergeTrees(t1.right, null);
-        
-            return root;
-        }
-        TreeNode left = mergeTrees(t1.left, t2.left);
-        TreeNode right = mergeTrees(t1.right, t2.right);
-        TreeNode root = new TreeNode(t1.val + t2.val);
-        root.left = left;
-        root.right = right;
-        return root;
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
     }
 
 }
