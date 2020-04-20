1297e0210489a9f671f37be6003ef6df9ac0872f
liuweijie <liuweijievip@gmail.com>
2020-04-20 23:01:50 +0800

    4.20

--- 572.另一个树的子树.java
+++ 572.另一个树的子树.java
@@ -74,7 +74,17 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null) return false;
+        return findSame(s,t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
 
+    public boolean findSame(TreeNode s, TreeNode t) {
+        if(s == null && t == null) return true;
+        if(s == null || t == null) return false;
+        if(s.val != t.val) return false;
+        boolean left = findSame(s.left, t.left);
+        boolean right = findSame(s.right, t.right);
+        return left && right;
     }
 }
 // @lc code=end
