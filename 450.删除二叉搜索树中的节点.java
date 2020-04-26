cbfd2b3abf97851c3ff192e8116739c3227fb2c0
liuweijie <liuweijievip@gmail.com>
2020-04-26 23:04:10 +0800

    4.26

--- 450.删除二叉搜索树中的节点.java
+++ 450.删除二叉搜索树中的节点.java
@@ -88,11 +88,7 @@ class Solution {
 
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null) return null;
-        if(root.val < key) {
-            root.right = deleteNode(root.right, key);
-        } else if(root.val > key) {
-            root.left = deleteNode(root.left, key);
-        } else if(root.val == key) {
+        if(root.val == key) {
             if(root.left == null && root.right == null) {
                 root = null;
             } else if(root.left != null) {
@@ -102,6 +98,10 @@ class Solution {
                 root.val = nextNode(root);
                 root.right = deleteNode(root.right, root.val);
             }
+        }else if(root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if(root.val > key) {
+            root.left = deleteNode(root.left, key);
         }
         return root;
     }
