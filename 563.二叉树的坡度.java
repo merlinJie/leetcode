cf40f1ae297c32a5097236d2507c3f008a3aceae
liuweijie <liuweijievip@gmail.com>
2020-04-28 23:25:36 +0800

    4.28

---
+++ 563.二叉树的坡度.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=563 lang=java
+ *
+ * [563] 二叉树的坡度
+ *
+ * https://leetcode-cn.com/problems/binary-tree-tilt/description/
+ *
+ * algorithms
+ * Easy (53.30%)
+ * Likes:    57
+ * Dislikes: 0
+ * Total Accepted:    9.1K
+ * Total Submissions: 17K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个二叉树，计算整个树的坡度。
+ * 
+ * 一个树的节点的坡度定义即为，该节点左子树的结点之和和右子树结点之和的差的绝对值。空结点的的坡度是0。
+ * 
+ * 整个树的坡度就是其所有节点的坡度之和。
+ * 
+ * 示例:
+ * 
+ * 
+ * 输入: 
+ * ⁠        1
+ * ⁠      /   \
+ * ⁠     2     3
+ * 输出: 1
+ * 解释: 
+ * 结点的坡度 2 : 0
+ * 结点的坡度 3 : 0
+ * 结点的坡度 1 : |2-3| = 1
+ * 树的坡度 : 0 + 0 + 1 = 1
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 任何子树的结点的和不会超过32位整数的范围。
+ * 坡度的值不会超过32位整数的范围。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int resule = 0;
+    public int findTilt(TreeNode root) {
+        if(root == null) return 0;
+        helpFind(root);
+        return resule;
+    }
+
+    public int helpFind(TreeNode root) {
+        if(root == null) return 0;
+        int left = helpFind(root.left);
+        int right = helpFind(root.right);
+        resule += Math.abs(left - right);
+        System.out.println(Math.abs(left - right));
+
+        return root.val + left + right;
+    }
+}
+// @lc code=end
+
