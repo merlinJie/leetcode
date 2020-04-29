20f7f261e7316ee66ad108f30456671ddc31293a
liuweijie <liuweijievip@gmail.com>
2020-04-29 23:03:55 +0800

    4.29

---
+++ 559.n叉树的最大深度.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=559 lang=java
+ *
+ * [559] N叉树的最大深度
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/description/
+ *
+ * algorithms
+ * Easy (69.12%)
+ * Likes:    84
+ * Dislikes: 0
+ * Total Accepted:    21.4K
+ * Total Submissions: 30.9K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 N 叉树，找到其最大深度。
+ * 
+ * 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+ * 
+ * 例如，给定一个 3叉树 :
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 我们应返回其最大深度，3。
+ * 
+ * 说明:
+ * 
+ * 
+ * 树的深度不会超过 1000。
+ * 树的节点总不会超过 5000。
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        
+    }
+}
+// @lc code=end
+
