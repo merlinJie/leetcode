0803d039f4abdbab8174deaa9455fad075721f68
liuweijie <liuweijievip@gmail.com>
2020-05-01 22:36:39 +0800

    5.0

---
+++ 590.n叉树的后序遍历.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=590 lang=java
+ *
+ * [590] N叉树的后序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (72.98%)
+ * Likes:    62
+ * Dislikes: 0
+ * Total Accepted:    21.3K
+ * Total Submissions: 29.1K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 N 叉树，返回其节点值的后序遍历。
+ * 
+ * 例如，给定一个 3叉树 :
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 返回其后序遍历: [5,6,3,2,4,1].
+ * 
+ * 
+ * 
+ * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        helpOrder(root, result);
+        return result;
+    }
+
+    public void helpOrder(Node root, List<Integer> list) {
+        if(root == null) return;
+        for(Node node: root.children) {
+            helpOrder(node, list);
+        }
+        list.add(root.val);
+    }
+}
+// @lc code=end
+
