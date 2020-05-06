52306944e9552f4a22978bee7ff36f6c3eff70e5
liuweijie <liuweijievip@gmail.com>
2020-05-06 22:38:23 +0800

    5.6

--- 232.用栈实现队列.java
+++ 232.用栈实现队列.java
@@ -45,33 +45,56 @@
 
 // @lc code=start
 class MyQueue {
-
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
     /** Initialize your data structure here. */
     public MyQueue() {
-
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
     }
     
     /** Push element x to the back of queue. */
     public void push(int x) {
-
+        stack1.add(x);
+        if(stack2.isEmpty()) {
+            while(!stack1.isEmpty()) {
+                int curInt = stack1.pop();
+                stack2.add(curInt);
+            }
+        }
     }
     
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-
+        if(!stack2.isEmpty()) {
+            return stack2.pop();
+        } else {
+            while(!stack1.isEmpty()) {
+                int curInt = stack1.pop();
+                stack2.add(curInt);
+            }
+            return stack2.pop();
+        }
     }
     
     /** Get the front element. */
     public int peek() {
-
+        if(!stack2.isEmpty()) {
+            return stack2.peek();
+        } else {
+            while(!stack1.isEmpty()) {
+                int curInt = stack1.pop();
+                stack2.add(curInt);
+            }
+            return stack2.peek();
+        }
     }
     
     /** Returns whether the queue is empty. */
     public boolean empty() {
-
+       return stack2.isEmpty() && stack1.isEmpty();
     }
 }
-
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
