9044c97232f6c284f6082f113aef622484c064b0
liuweijie <liuweijievip@gmail.com>
2020-05-20 00:04:10 +0800

    5.19

--- 165.比较版本号.java
+++ 165.比较版本号.java
@@ -67,7 +67,25 @@
 // @lc code=start
 class Solution {
     public int compareVersion(String version1, String version2) {
-
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int max = Math.max(v1.length, v2.length);
+        for (int i = 0; i < max; i++) {
+            int iV1 = 0;
+            int iV2 = 0;
+            if(v1.length > i) {
+                iV1 = Integer.parseInt(v1[i]); 
+            }
+            if(v2.length > i) {
+                iV2 = Integer.parseInt(v2[i]); 
+            }
+            if(iV1 > iV2) {
+                return 1;
+            } else if(iV1 < iV2 ) {
+                return -1;
+            }
+        }
+        return 0;
     }
 }
 // @lc code=end
