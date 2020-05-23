e5eb761cb545a5bc2f5f38ca9057cbbc6180dd96
liuweijie <liuweijievip@gmail.com>
2020-05-23 22:55:21 +0800

    5.21

---
+++ 653.两数之和-iv-输入-bst.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=653 lang=java
+ *
+ * [653] 两数之和 IV - 输入 BST
+ *
+ * https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/description/
+ *
+ * algorithms
+ * Easy (54.77%)
+ * Likes:    123
+ * Dislikes: 0
+ * Total Accepted:    13.7K
+ * Total Submissions: 24.9K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n9'
+ *
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ * 
+ * 案例 1:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 9
+ * 
+ * 输出: True
+ * 
+ * 
+ * 
+ * 
+ * 案例 2:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 28
+ * 
+ * 输出: False
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> sets = new ArrayList<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+        if(sets.contains(k - root.val)) {
+            return true;
+        }
+        sets.add(root.val);
+        return findTarget(root.left, k) || findTarget(root.right, k);
+    }
+}
+// @lc code=end
+
