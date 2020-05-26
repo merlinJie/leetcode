31c6521afb1dcc9c7521103958345a236126e51a
liuweijie <liuweijievip@gmail.com>
2020-05-26 23:41:17 +0800

    5.26

--- 160.相交链表.java
+++ 160.相交链表.java
@@ -87,8 +87,16 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
+       ListNode pA = headA;
+       ListNode pB = headB;
+
+       while(pA != pB) {
+           pA = pA != null ? pA.next: headB;
+           pB = pB != null ? pB.next: headA;
+       }
+
+       return pA;
     }
-}
+}}
 // @lc code=end
 
