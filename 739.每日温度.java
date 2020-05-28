23cb24c90137749e2a9911b5b974aa47f89f8b47
liuweijie <liuweijievip@gmail.com>
2020-05-28 22:05:41 +0800

    5.27

--- 739.每日温度.java
+++ 739.每日温度.java
@@ -25,7 +25,16 @@
 // @lc code=start
 class Solution {
     public int[] dailyTemperatures(int[] T) {
-
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[T.length];
+        for(int i = 0; i < T.length; i++) {
+            while(!stack.isEmpty() && T[stack.peek()] < T[i]) {
+                res[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        return res;
     }
 }
 // @lc code=end
