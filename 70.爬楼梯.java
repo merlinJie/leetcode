a062a680dfadee8b2c561e9f350f35cc7b7f3d21
merlin <liuweijievip@gmail.com>
2020-05-28 17:37:07 +0800

    5.28

--- 70.爬楼梯.java
+++ 70.爬楼梯.java
@@ -42,7 +42,18 @@
 // @lc code=start
 class Solution {
     public int climbStairs(int n) {
-
+        int dpTable[] = new int[n + 1];
+        if(n == 0) return 0;
+        if(n == 1) {
+            return 1;
+        }
+        if(n == 2) return 2;
+        dpTable[1] = 1;
+        dpTable[2] = 2;
+        for(int i = 3; i <= n; i++) {
+            dpTable[i] = dpTable[i - 1] + dpTable[i - 2];
+        }
+        return dpTable[n];
     }
 }
 // @lc code=end
