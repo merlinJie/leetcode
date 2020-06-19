ec5ef553d1c563b022806ea8459dc30a58d73cf1
liuweijie <liuweijievip@gmail.com>
2020-06-20 00:39:50 +0800

    6.20

--- 35.搜索插入位置.java
+++ 35.搜索插入位置.java
@@ -46,7 +46,27 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0) return 0;
+        int left = 0;
+        int right = nums.length - 1;
+        if (target > nums[right]) {
+            return nums.length;
+        }
+        if (target < nums[0]) {
+            return 0;
+        }
 
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
 // @lc code=end
