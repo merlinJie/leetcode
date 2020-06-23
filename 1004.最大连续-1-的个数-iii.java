c6fa8ef49b01521301380cfc3216172045646905
liuweijie <liuweijievip@gmail.com>
2020-06-23 09:43:54 +0800

    6.23

--- 1004.最大连续-1-的个数-iii.java
+++ 1004.最大连续-1-的个数-iii.java
@@ -50,7 +50,25 @@
 // @lc code=start
 class Solution {
     public int longestOnes(int[] A, int K) {
-
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int max = 0;
+        while (right < A.length) {
+            if(A[right] == 0) {
+                count++;
+            }
+            right++;
+            while (count > K) {
+                int temp = A[left];
+                left++;
+                if (temp == 0) {
+                    count--;
+                }
+            }
+            max = Math.max(max, right - left);
+        }
+        return max;
     }
 }
 // @lc code=end
